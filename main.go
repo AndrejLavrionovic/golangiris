cb86110db7c725df98bfa184aea87bca7c0f4a71
manag_000 <andrei.lavrinovic@gmail.com>
2016-11-11 00:11:15 +0000

    iris > listener

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"net"
+	"github.com/kataras/iris"
+)
 
 func main() {
-	iris.Get("/", hi)
-	iris.Listen(":9999")
+	iris.Get("/", func(ctx *iris.Context){
+		ctx.Render("hi.html", struct {Name string}{Name: "Welcom"})
+	})
+	iris.Get("/server2", server2)
+
+	ln, err := net.Listen("tcp4", ":9898")
+	if err != nil{
+		panic(err)
+	}
+
+	iris.Serve(ln)
 }
 
-func hi(ctx *iris.Context){
-	ctx.Render("hi.html", struct { Name string }{ Name: "Andrej."})
+func server2(ctx *iris.Context){
+	ctx.Render("hi.html", struct { Name string }{ Name: "Welcome to secure server #2"})
 }
