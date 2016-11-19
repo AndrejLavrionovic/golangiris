51287cbcde7fe58e220de49ea6f494ec316c6068
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-11-19 17:34:32 +0000

    index.html

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	iris.Get("/", func(ctx *iris.Context){
-		ctx.Render("hi.html", struct {Name string}{Name: "Welcom"})
+		ctx.Write(ctx.Request.URI().String())
+	})
+
+	iris.Get("/server1", func(ctx *iris.Context){
+		ctx.Render("hi.html", struct{Name string}{Name: "Welcom"})
 	})
 	iris.Get("/server2", server2)
 
-	ln, err := net.Listen("tcp4", ":9898")
+	ln, err := net.Listen("tcp4", "127.0.0.1:9898")
 	if err != nil{
 		panic(err)
 	}
